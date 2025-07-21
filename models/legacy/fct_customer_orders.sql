a12b0c547f606597f8fd22162dd946c7e7760bd3
Biosurfer PK <praveenkeshav707@gmail.com>
2025-07-21 06:59:44 +0000

    pull out intermediates and simple subqueries

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -33,6 +33,17 @@ payments as (
 --Final CTEs
 --Simple Select Statement
 
+completed_payments as(
+
+    select
+        orderid as order_id,
+        max(created) as payment_finalized_date,
+        sum(amount) / 100.0 as total_amount_paid
+    from payments
+    where status <> 'fail' 
+    group by 1
+
+),
 
 paid_orders as (
     select orders.id as order_id,
@@ -80,12 +91,40 @@ select
     left join customer_orders as c using (customer_id)
     left outer join 
     (
-        select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
+
+---Final CTE    
+
+final as (
+
+    select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
+        p.*,
+        row_number() over (order by p.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+        case when c.first_order_date = p.order_placed_at
+        then 'new'
+        else 'return' end as nvsr,
+        x.clv_bad as customer_lifetime_value,
+        c.first_order_date as fdos
+    from paid_orders p
+        left join customer_orders as c using (customer_id)
+        left join
+        (
+            select
+              p.order_id
+              sum(t2.total_amount_paid) as clv_bad
+            from paid_orders p
+            left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+            group by 1
+            order by p.order_id
     ) x on x.order_id = p.order_id
     order by order_id
+
+)
+
+-- Simple Select Statment
+
+select * from final
+                
+
